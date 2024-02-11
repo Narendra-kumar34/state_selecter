afb3e96fcdc734dd1392f85450b8e3ac92ab9804
Narendra <narendrach34@gmail.com>
2024-02-11 15:49:42 +0530

    implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://crio-location-selector.onrender.com/countries")
+      .then((countries) => setCountries(countries.data))
+      .catch((err) => console.log("Error fetching countries:", err));
+  }, []);
+
+  useEffect(() => {
+    if (selectedCountry) {
+      axios
+        .get(
+          `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
+        )
+        .then((states) => {
+          setStates(states.data);
+          setSelectedState("");
+          setCities([]);
+          setSelectedCity("");
+        })
+        .catch((err) => console.log("Error fetching states:", err));
+    }
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    if (selectedCountry && selectedState) {
+      axios
+        .get(
+          `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+        )
+        .then((cities) => {
+          setCities(cities.data);
+          setSelectedCity("");
+        })
+        .catch((err) => console.log("Error fetching cities:", err));
+    }
+  }, [selectedCountry, selectedState]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Select Location</h1>
+      <div className="dropdowns">
+        <select
+          value={selectedCountry}
+          onChange={(e) => setSelectedCountry(e.target.value)}
+          className="dropdown"
         >
-          Learn React
-        </a>
-      </header>
+          <option value="" disabled>
+            Select Country
+          </option>
+          {countries.map((country) => {
+            return (
+              <option value={country} key={country}>
+                {country}
+              </option>
+            );
+          })}
+        </select>
+        <select
+          value={selectedState}
+          onChange={(e) => setSelectedState(e.target.value)}
+          className="dropdown"
+        >
+          <option value="" disabled>
+            Select State
+          </option>
+          {states.map((state) => {
+            return (
+              <option value={state} key={state}>
+                {state}
+              </option>
+            );
+          })}
+        </select>
+        <select
+          value={selectedCity}
+          onChange={(e) => setSelectedCity(e.target.value)}
+          className="dropdown"
+        >
+          <option value="" disabled>
+            Select City
+          </option>
+          {cities.map((city) => {
+            return (
+              <option value={city} key={city}>
+                {city}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {selectedCity && (
+        <h2 className="result">
+          You selected <span className="highlight">{selectedCity}</span>,
+          <span className="fade">
+            {" "}
+            {selectedState}, {selectedCountry}
+          </span>
+        </h2>
+      )}
     </div>
   );
 }
